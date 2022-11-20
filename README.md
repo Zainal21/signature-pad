be8112f7e2e482227a9ce6e26ff6a8db21ec0660
Zainal21 <Zainalarifin080718@gmail.com>
2022-11-20 12:59:05 +0700

    docs : add general documentation

---
+++ README.md
@@ -0,0 +1,35 @@
+# Signature Pad (Experiment Apps)
+
+## Prerequiste
+
+- [Kotlin](https://kotlinlang.org//)
+- Android Studio
+
+## Stacks
+
+- Kotlin
+- gcacace:signature-pad
+
+(Running inside Container)
+
+## Installation
+
+1. Clone repository
+
+```bash
+$  git clone git remote add origin https://github.com/Zainal21/signature-pad.git
+```
+
+2. open your android studion and run
+
+## Example Result
+
+- Splash Screen
+  ![sign pad](public/splash_screen.png)
+
+- Sign Pad
+  ![sign pad](public/signature_pad.png)
+
+Copyright © 2022 by Muhamad Zainal Arifin
+
+> The Project is Under MIT licensed.
--- app/src/main/java/com/muhamadzain/signature_pad/ui/MainActivity.kt
+++ app/src/main/java/com/muhamadzain/signature_pad/ui/MainActivity.kt
@@ -1,31 +1,106 @@
 package com.muhamadzain.signature_pad.ui
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
+import android.os.Environment
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
 import com.github.gcacace.signaturepad.views.SignaturePad
 import com.muhamadzain.signature_pad.R
 import com.muhamadzain.signature_pad.base.BaseActivity
+import java.io.*
+
 
 class MainActivity : BaseActivity() {
 
     private lateinit var mSignaturePad : SignaturePad
-    private lateinit var bSaveSignature : Button
-    private lateinit var bClearSIgnature : Button
+    private lateinit var bSaveSignature : AppCompatButton
+    private lateinit var bClearSignature : AppCompatButton
+
+    private val REQUEST_EXTERNAL_STORAGE = 1
+    private val PERMISSIONS_STORAGE = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    override fun getLayoutResource(): Int = R.layout.activity_main
 
     private fun initComponents() {
         bSaveSignature = findViewById(R.id.save_signature)
-        bClearSIgnature = findViewById(R.id.clear_button)
+        bClearSignature = findViewById(R.id.clear_button)
         mSignaturePad = findViewById(R.id.signature_pad)
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_EXTERNAL_STORAGE -> {
+                if (grantResults.size!! <= 0
+                    || grantResults[0] !== PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Cannot write images to external storage",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
     private fun initEventListener(){
         bSaveSignature.setOnClickListener {
-            Toast.makeText(this@MainActivity, "This is work", Toast.LENGTH_SHORT).show()
+            val signatureBitmap = mSignaturePad.signatureBitmap
+            if (addJpgSignatureToGallery(signatureBitmap)) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Unable to store the signature",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            if (addSvgSignatureToGallery(mSignaturePad.signatureSvg)) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "SVG Signature saved into the Gallery",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Unable to store the SVG signature",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
 
-        bClearSIgnature.setOnClickListener {
-            Toast.makeText(this@MainActivity, "This is work", Toast.LENGTH_SHORT).show()
+        bClearSignature.setOnClickListener {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setMessage("Are you sure you want to Delete this signature?")
+                .setCancelable(false)
+                .setPositiveButton("Yes") { dialog, id ->
+                    mSignaturePad.clear()
+                }
+                .setNegativeButton("No") { dialog, id ->
+                    dialog.dismiss()
+                }
+            val alert = builder.create()
+            alert.show()
         }
 
         mSignaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
@@ -33,16 +108,98 @@ class MainActivity : BaseActivity() {
 
             override fun onSigned() {}
 
-            override fun onClear() {}
+            override fun onClear() {
+                Toast.makeText(this@MainActivity, "Signature was clear", Toast.LENGTH_SHORT).show()
+            }
         })
     }
 
-    override fun getLayoutResource(): Int = R.layout.activity_main
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        verifyStoragePermissions(this)
+        setContentView(R.layout.activity_main)
         initComponents()
         initEventListener()
-        setContentView(R.layout.activity_main)
     }
+
+    private fun verifyStoragePermissions(activity: Activity?) {
+        val permission = ActivityCompat.checkSelfPermission(
+            activity!!,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                activity,
+                PERMISSIONS_STORAGE,
+                REQUEST_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+
+    private fun getAlbumStorageDir(albumName: String?): File {
+        val file = File(
+            Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+            ), albumName
+        )
+        if (!file.mkdirs()) {
+            Log.e("SignaturePad", "Directory not created")
+        }
+        return file
+    }
+
+    private fun saveBitmapToJPG(bitmap: Bitmap, photo: File?) {
+        val newBitmap = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(newBitmap)
+        canvas.drawColor(Color.WHITE)
+        val stream: OutputStream = FileOutputStream(photo)
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
+        stream.close()
+    }
+
+    private fun addJpgSignatureToGallery(signature: Bitmap): Boolean {
+        var result = false
+        try {
+            val photo = File(
+                getAlbumStorageDir("SignaturePad"),
+                String.format("Signature_%d.jpg", System.currentTimeMillis())
+            )
+            saveBitmapToJPG(signature, photo)
+            scanMediaFile(photo)
+            result = true
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
+    private fun scanMediaFile(photo: File) {
+        val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+        val contentUri: Uri = Uri.fromFile(photo)
+        mediaScanIntent.data = contentUri
+        this@MainActivity.sendBroadcast(mediaScanIntent)
+    }
+
+    fun addSvgSignatureToGallery(signatureSvg: String?): Boolean {
+        var result = false
+        try {
+            val svgFile = File(
+                getAlbumStorageDir("SignaturePad"),
+                String.format("Signature_%d.svg", System.currentTimeMillis())
+            )
+            val stream: OutputStream = FileOutputStream(svgFile)
+            val writer = OutputStreamWriter(stream)
+            writer.write(signatureSvg)
+            writer.close()
+            stream.flush()
+            stream.close()
+            scanMediaFile(svgFile)
+            result = true
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return result
+    }
+
 }
